238ded448de800b8a31f5c02411eed0604a8d9c2
arijit sahoo <tijira1999sahoo@gmail.com>
2019-08-06 23:29:44 +0530

    converting file path and feed to imageio

--- converter.py
+++ converter.py
@@ -1,3 +1,20 @@
 import imageio
 import os
 
+clip  = os.path.abspath('me.mp4')
+
+# print (clip)
+
+def gifMaker(inputPath, targetFormat):
+    outputPath = os.path.splitext(inputPath)[0] + targetFormat
+
+    print(f'converting {inputPath} \n {outputPath}')
+
+    reader = imageio.get_reader(inputPath)
+    fps = reader.get_meta_data()['fps']
+
+    writer = imageio.get_writer(outputPath, fps=fps)
+
+    for frames in reader:
+        writer.append_data(frames)
+        
