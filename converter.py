2f8807e0fd5baa0e5ce0016ec0aa6b2b5e29fdc2
arijit sahoo <tijira1999sahoo@gmail.com>
2019-08-06 23:37:41 +0530

    final build

--- converter.py
+++ converter.py
@@ -15,6 +15,13 @@ def gifMaker(inputPath, targetFormat):
 
     writer = imageio.get_writer(outputPath, fps=fps)
 
-    for frames in reader:
+    for frames in reader:       # extract all frames and write in gif file
         writer.append_data(frames)
-        
+        print(f'Frame {frames}')
+    
+    print('Done!')
+    writer.close()
+
+
+gifMaker(clip, '.gif') # functio call to gifmaker with the var containing file path and target format type that is gif
+
